4d902cf9e78ace3ca0d2b51c966ed762b8c3cec9
Xiangxu <iasonliu@gmail.com>
2021-11-03 15:49:09 +0800

    add invaders

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ use std::{
 
 use invaders::{
     frame::{self, new_frame, Drawable},
+    invaders::Invaders,
     player::Player,
     render,
 };
@@ -53,6 +54,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     // Game Loop
     let mut player = Player::new();
     let mut instant = Instant::now();
+    let mut invaders = Invaders::new();
     'gameloop: loop {
         // Per-frame init
         let delta = instant.elapsed();
@@ -80,8 +82,14 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
         // Updates
         player.update(delta);
+        if invaders.update(delta) {
+            audio.play("move");
+        }
         // Draw & render
-        player.draw(&mut curr_frame);
+        let drawable: Vec<&dyn Drawable> = vec![&player, &invaders];
+        for drawable in drawable {
+            drawable.draw(&mut curr_frame)
+        }
         let _ = render_tx.send(curr_frame);
         thread::sleep(Duration::from_millis(1));
     }
