138a82339ef97b3b75052d282d3ea3275e4448f6
Xiangxu <iasonliu@gmail.com>
2021-11-03 14:15:37 +0800

    add player

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,8 @@ use std::{
 };
 
 use invaders::{
-    frame::{self, new_frame, Drawable, Frame},
+    frame::{self, new_frame, Drawable},
+    player::Player,
     render,
 };
 
@@ -50,14 +51,17 @@ fn main() -> Result<(), Box<dyn Error>> {
     });
 
     // Game Loop
+    let mut player = Player::new();
     'gameloop: loop {
         // Per-frame init
-        let curr_frame = new_frame();
+        let mut curr_frame = new_frame();
 
         //Input
         while event::poll(Duration::default())? {
             if let Event::Key(key_event) = event::read()? {
                 match key_event.code {
+                    KeyCode::Left => player.move_left(),
+                    KeyCode::Right => player.move_right(),
                     KeyCode::Esc | KeyCode::Char('q') => {
                         audio.play("lose");
                         break 'gameloop;
@@ -67,6 +71,7 @@ fn main() -> Result<(), Box<dyn Error>> {
             }
         }
         // Draw & render
+        player.draw(&mut curr_frame);
         let _ = render_tx.send(curr_frame);
         thread::sleep(Duration::from_millis(1));
     }
