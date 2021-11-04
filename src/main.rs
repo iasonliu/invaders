29dbbefd02ef2911ea5dadac88e8a9cef979d01e
Xiangxu <iasonliu@gmail.com>
2021-11-04 10:47:27 +0800

    add win or lose

--- src/main.rs
+++ src/main.rs
@@ -85,6 +85,9 @@ fn main() -> Result<(), Box<dyn Error>> {
         if invaders.update(delta) {
             audio.play("move");
         }
+        if player.detect_hits(&mut invaders) {
+            audio.play("explode");
+        }
         // Draw & render
         let drawable: Vec<&dyn Drawable> = vec![&player, &invaders];
         for drawable in drawable {
@@ -92,6 +95,14 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
         let _ = render_tx.send(curr_frame);
         thread::sleep(Duration::from_millis(1));
+        // Win or lose?
+        if invaders.all_killed() {
+            audio.play("win");
+            break 'gameloop;
+        }
+        if invaders.reached_bottom() {
+            audio.play("lose");
+        }
     }
 
     // Cleanup
--- src/player.rs
+++ src/player.rs
@@ -1,5 +1,6 @@
 use crate::{
     frame::{Drawable, Frame},
+    invaders::Invaders,
     shot::Shot,
     {NUM_COLS, NUM_ROWS},
 };
@@ -43,6 +44,16 @@ impl Player {
             shot.update(delta);
         }
         self.shots.retain(|shot| !shot.dead());
+    }
+    pub fn detect_hits(&mut self, invaders: &mut Invaders) -> bool {
+        let mut hit_something = false;
+        for shot in self.shots.iter_mut() {
+            if invaders.kill_invader_at(shot.x, shot.y) {
+                hit_something = true;
+                shot.explode();
+            }
+        }
+        hit_something
     }
 }
 
